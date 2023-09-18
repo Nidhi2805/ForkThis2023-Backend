0ccc92b55b18a5e3a1b02409ebad1a2fb65e06b8
woaitsAryan <yoprince.ab@gmail.com>
2023-09-18 22:33:18 +0530

    small changes

--- src/managers/db.ts
+++ src/managers/db.ts
@@ -4,13 +4,14 @@ import logger from '../../logs/logger.js';
 
 const URL: string = envHandler("DATABASE_URL");
 
+console.log(URL);
 const connectToDB = (): Promise<void> => mongoose.connect(URL)
-    .then(() => {
-        console.log("Connected to Database!");
-    })
-    .catch (error => {
-        console.log(error)
-        logger.error(error);
+.then(() => {
+    console.log("Connected to Database!");
+})
+.catch (error => {
+    console.log(error);
+    logger.error(error);
     });
 
 export default connectToDB;
