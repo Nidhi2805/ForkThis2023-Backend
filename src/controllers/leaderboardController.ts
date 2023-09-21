7e2ea5ff88bee786278a6bd1847c3fdd0dea97b3
woaitsAryan <yoprince.ab@gmail.com>
2023-09-21 21:45:58 +0530

    added counter to leaderboard

--- src/controllers/leaderboardController.ts
+++ src/controllers/leaderboardController.ts
@@ -6,11 +6,10 @@ export const getLeaderboard = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
         const {limit, page} = req.query;
         const leaderboard = await User.find().sort({score: -1}).limit(Number(limit)).skip(Number(page));
-
+        const count = await User.countDocuments();
         const leaderboardWithIndex = leaderboard.map((user, index) => ({
             ...user.toObject(),
             rank: index + 1 + (Number(page) * Number(limit)),
           }));
-        return res.status(200).json({"success":true, "data":leaderboardWithIndex});
+        return res.status(200).json({"count":count, "success":true, "data":leaderboardWithIndex});
 });
-
