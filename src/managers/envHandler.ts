0680721f401b6bde8d236cf81658378cfa3097fd
Nidhi2805 <nidhianand.karva2023@vitstudent.ac.in>
2024-09-19 09:23:29 +0530

    feat: Git Outh

--- src/managers/envHandler.ts
+++ src/managers/envHandler.ts
@@ -1,8 +1,7 @@
-import 'dotenv/config';
+import 'dotenv/config'; // Ensure environment variables from .env are loaded
 
-// Generic environment handler
 const envHandler = (envName: string): string => {
-    const env = process.env[envName];
+    const env = process.env[envName]; // Access environment variable from process.env
     if (!env) throw new Error(`ENV ${envName} is not defined.`);
     return env;
 };
